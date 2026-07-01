9694fb2e8c044e43a24a8f9d61840e7722cc696d
Karma3843 <ahershantanu04@gmail.com>
2026-07-01 11:41:38 +0530

    Changes in database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,12 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL="postgresql://postgres:12345678@localhost:5432/Family_Database"
+load_dotenv()
+
+DATABASE_URL= os.getenv("DATABASE_URL")
 
 engine=create_engine(DATABASE_URL)
 
